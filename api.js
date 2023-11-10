8b7e08414021a5e9810cd3526b92369c2ee75c37
Alex Turlaykov <124469993+alekseiTurl@users.noreply.github.com>
2023-11-10 15:40:49 +0300

    Update api.js

--- api.js
+++ api.js
@@ -1,16 +1,1 @@
-JSON.stringify({
-    [
-    detail1: {
-        title: 'Some title detail 1',
-        description: "Some descr on detail 1 Some descr on detail 1 Some descr on detail 1 Some descr on detail 1 Some descr on detail 1 Some descr on detail 1",
-        images: ['https://alekseiturl.github.io/Web-Foundation/img/alextur.webp', 'https://alekseiturl.github.io/Web-Foundation/img/alexicon.webp'],
-        link: 'https://alekseiturl.github.io/Boostrap-Skillbox/'
-    },
-    detail2: {
-        title: 'Some title detail 2',
-        description: "Some descr on detail 2 Some descr on detail 2 Some descr on detail 2 Some descr on detail 2 Some descr on detail 2 Some descr on detail 2",
-        images: ['https://alekseiturl.github.io/Web-Foundation/img/alextur.webp', 'https://alekseiturl.github.io/Web-Foundation/img/alexicon.webp'],
-        link: 'https://alekseiturl.github.io/Evclid/'
-    }
-]
-})
+{"detail1":{"title":"Some title detail 1","description":"Some descr on detail 1 Some descr on detail 1 Some descr on detail 1 Some descr on detail 1 Some descr on detail 1 Some descr on detail 1","images":["https://alekseiturl.github.io/Web-Foundation/img/alextur.webp","https://alekseiturl.github.io/Web-Foundation/img/alexicon.webp"],"link":"https://alekseiturl.github.io/Boostrap-Skillbox/"},"detail2":{"title":"Some title detail 2","description":"Some descr on detail 2 Some descr on detail 2 Some descr on detail 2 Some descr on detail 2 Some descr on detail 2 Some descr on detail 2","images":["https://alekseiturl.github.io/Web-Foundation/img/alextur.webp","https://alekseiturl.github.io/Web-Foundation/img/alexicon.webp"],"link":"https://alekseiturl.github.io/Evclid/"}}
