f71b106b198ede07367679bfe97b80061599a3e4
Alex Turlaykov <124469993+alekseiTurl@users.noreply.github.com>
2023-11-10 12:38:54 +0300

    Create api.js

---
+++ api.js
@@ -0,0 +1,15 @@
+const data = {
+    detail1: {
+        title: 'Some title detail 1',
+        description: "Some descr on detail 1 Some descr on detail 1 Some descr on detail 1 Some descr on detail 1 Some descr on detail 1 Some descr on detail 1",
+        images: ['https://alekseiturl.github.io/Web-Foundation/img/alextur.webp', 'https://alekseiturl.github.io/Web-Foundation/img/alexicon.webp'],
+        link: 'https://alekseiturl.github.io/Boostrap-Skillbox/'
+    },
+    detail2: {
+        title: 'Some title detail 2',
+        description: "Some descr on detail 2 Some descr on detail 2 Some descr on detail 2 Some descr on detail 2 Some descr on detail 2 Some descr on detail 2",
+        images: ['https://alekseiturl.github.io/Web-Foundation/img/alextur.webp', 'https://alekseiturl.github.io/Web-Foundation/img/alexicon.webp'],
+        link: 'https://alekseiturl.github.io/Evclid/'
+    }
+};
+JSON.stringify(data);
